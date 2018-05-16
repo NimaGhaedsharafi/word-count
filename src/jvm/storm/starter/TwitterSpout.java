902b2b613a0d00621528648d7a5548ed6adea55c
Nima Ghaedsharafi <>
2018-05-16 20:37:14 +0430

    sign the contract with BaseRichSpout

--- src/jvm/storm/starter/TwitterSpout.java
+++ src/jvm/storm/starter/TwitterSpout.java
@@ -1,6 +1,25 @@
 package storm.starter;
 
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichSpout;
 
+import java.util.Map;
+
 public class TwitterSpout extends BaseRichSpout {
+    @Override
+    public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
+
+    }
+
+    @Override
+    public void nextTuple() {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
+    }
 }
