c40c95a1aa42ef5cdb9e8492418335ad1a2e5779
Nima Ghaedsharafi <>
2018-05-16 21:04:27 +0430

    let's define the output fields

--- src/jvm/storm/starter/WordSplitterBolt.java
+++ src/jvm/storm/starter/WordSplitterBolt.java
@@ -4,6 +4,7 @@ import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 import twitter4j.Status;
@@ -30,6 +31,6 @@ public class WordSplitterBolt extends BaseRichBolt {
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
-
+        outputFieldsDeclarer.declare(new Fields("word"));
     }
 }
