87d5c3c6b9cabd8138661b2b9420fb0a491790b6
Nima Ghaedsharafi <>
2018-05-16 20:52:25 +0430

    we just need to pass the tweet field

--- src/jvm/storm/starter/TwitterSpout.java
+++ src/jvm/storm/starter/TwitterSpout.java
@@ -4,6 +4,7 @@ import org.apache.storm.spout.SpoutOutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 import org.apache.storm.utils.Utils;
 import twitter4j.*;
@@ -73,6 +74,6 @@ public class TwitterSpout extends BaseRichSpout {
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
-
+        outputFieldsDeclarer.declare(new Fields("tweet"));
     }
 }
