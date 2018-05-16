ba2603b7048cdb3b663e09d4b53f1a90c3859c3f
Nima Ghaedsharafi <>
2018-05-16 21:12:44 +0430

    keep eyes on what we've done at our past

--- src/jvm/storm/starter/StatisticsBolt.java
+++ src/jvm/storm/starter/StatisticsBolt.java
@@ -6,12 +6,20 @@ import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Tuple;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class StatisticsBolt extends BaseRichBolt {
+
+    private Map<String, Long> counter;
+    private long lastLogTime;
+    private long lastClearTime;
+
     @Override
     public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
-        
+        counter = new HashMap<String, Long>();
+        lastLogTime = System.currentTimeMillis();
+        lastClearTime = System.currentTimeMillis();
     }
 
     @Override
