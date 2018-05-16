bb526402005d63394d96ff6f9d1ce9fa73c1a0f4
Nima Ghaedsharafi <>
2018-05-16 21:52:37 +0430

    let's get rid of some nonsense

--- src/jvm/storm/starter/IgnoranceBolt.java
+++ src/jvm/storm/starter/IgnoranceBolt.java
@@ -7,11 +7,18 @@ import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class IgnoranceBolt extends BaseRichBolt {
 
     private OutputCollector collector;
+
+    private Set<String> IGNORE_LIST = new HashSet<>(Arrays.asList("http", "https", "the", "you", "que", "and", "for", "that", "like", "have", "this", "just", "with", "all", "get",
+            "about", "can", "was", "not", "your", "but", "are", "one", "what", "out", "when", "get", "lol", "now", "para", "por",
+            "want", "will", "know", "good", "from", "las", "don", "people", "got", "why", "con", "time", "would"));
 
     @Override
     public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
