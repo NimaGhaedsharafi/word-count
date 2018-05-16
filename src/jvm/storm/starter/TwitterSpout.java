1cb38ec67acabdf439fa507fd67898cbf33e1a30
Nima Ghaedsharafi <>
2018-05-16 20:46:04 +0430

    add some sample code to open a twitter stream

--- src/jvm/storm/starter/TwitterSpout.java
+++ src/jvm/storm/starter/TwitterSpout.java
@@ -4,13 +4,53 @@ import org.apache.storm.spout.SpoutOutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichSpout;
+import twitter4j.*;
 
 import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class TwitterSpout extends BaseRichSpout {
+
+    private SpoutOutputCollector collector;
+    private LinkedBlockingQueue<Status> queue;
+    private TwitterStream twitterStream;
+
     @Override
     public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
+        this.queue = new LinkedBlockingQueue<>(1000);
+        this.collector = spoutOutputCollector;
 
+        StatusListener listener = new StatusListener() {
+            @Override
+            public void onStatus(Status status) {
+                queue.offer(status);
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice sdn) {
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int i) {
+            }
+
+            @Override
+            public void onScrubGeo(long l, long l1) {
+            }
+
+            @Override
+            public void onStallWarning(StallWarning stallWarning) {
+            }
+
+            @Override
+            public void onException(Exception e) {
+            }
+        };
+
+        TwitterStreamFactory factory = new TwitterStreamFactory();
+        this.twitterStream = factory.getInstance();
+        this.twitterStream.addListener(listener);
+        this.twitterStream.sample();
     }
 
     @Override
